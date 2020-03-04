803ea789a4ef35bff6afcc2e517f4e2ea1130a32
Xavier Gimenez <xgimenez@disgrafic.com>
2020-03-04 08:24:38 +0100

    Updates

---
+++ precache-manifest.35190b8be48506c58efa3d81e3da66d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03f475b2936018e85ac9b0e85bd5cbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad7a0640a80b277db40e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "52681634e3551afe07ed",
+    "url": "/static/js/2.92030a85.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.92030a85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad7a0640a80b277db40e",
+    "url": "/static/js/main.0df9170e.chunk.js"
+  },
+  {
+    "revision": "8856bbe1298527678be0",
+    "url": "/static/js/runtime-main.5775a872.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
