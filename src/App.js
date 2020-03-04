cef3db49b137f762c0f99a7b2084e638a8d574b5
Xavier Gimenez <xgimenez@disgrafic.com>
2020-03-04 10:34:32 +0100

    add dummy funcionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function App({}) {
+  const [dummy, setDummy] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +12,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input id="intro" onChange={e=> setDummy(e.target.value)} type="number" />
+        <p>{dummy}</p>
       </header>
     </div>
   );
